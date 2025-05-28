d03ddc993b009cd89f900d4e40d131dadaea07cb
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-05-28 11:29:28 -0300

    Update admin 222

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_URL = "https://newstylegym-back.onrender.com;
+const API_URL = "https://newstylegym-back.onrender.com";
 
 const Admin = () => {
   const getCurrentMonthName = () => {
