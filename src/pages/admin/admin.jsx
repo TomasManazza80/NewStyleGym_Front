4681a5576785c80f05ed48960eb439c0ecc94f71
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-05-28 11:27:38 -0300

    admin relase

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://newstylegym-back.onrender.com;
 
 const Admin = () => {
   const getCurrentMonthName = () => {
